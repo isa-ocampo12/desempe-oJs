877e1eceb201298dbc992631716efcdd87a6c070
isa-ocampo12 <isaocampo174@gmail.com>
2024-10-29 19:05:33 -0500

    Se crea funcion inicio de sesion y se relaciona con el boton de iniciar

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+let intento = 0
+
+function iniciarSesion(){
+    if(intento >= 3){
+        console.log('Llegaste al maximo numero de intentos, sistema cerrado')
+        return
+    }
+    let usuario = document.querySelector("#usuario").value
+    let contraseña = document.querySelector("#contraseña").value
+
+    if(usuario == 'admin' && contraseña == '1234'){
+        console.log('Bienvenido')
+        
+
+    }else {
+        intento++            
+        console.log('Por favor intente nuevamente')
+    }
+}
+
+
 
 
 
